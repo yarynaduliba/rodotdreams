47c166b5c0a47122347dea68e2d2762f5c93d069
dulibatest <duliba@qarea.us>
2025-09-29 11:08:44 +0300

    for myself

---
+++ lesson3/src/lesson/lesson4/objects.js
@@ -0,0 +1,11 @@
+const obj = {};
+
+obj.key1 = 'val1';
+obj['key2'] = 'val2';
+obj['key3 with space'] = 'val3 with saces';
+obj.key4 = {
+    subkey1: 1,
+    subkey2: '2',
+    subkey3: true,
+    subkey4: []
+}
